678db9772ba0bad2efa99fbbfbfbcce8f4a01993
Nikita Khrustalev <nikita.khrustalev2012@gmail.com>
2023-06-09 20:55:23 +0300

    feat: migrate news to TS

---
+++ migration-newip-to-ts/src/types/index.ts
@@ -0,0 +1,13 @@
+export interface INews {
+    author: string;
+    content: string;
+    description: string;
+    publishedAt: string;
+    source: {
+        id: string;
+        name: string;
+    };
+    title: string;
+    url: string;
+    urlToImage: string;
+}
