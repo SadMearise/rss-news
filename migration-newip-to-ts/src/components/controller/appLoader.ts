47e7b397e535ced16293a6ca89a89b26734e7d47
Nikita Khrustalev <nikita.khrustalev2012@gmail.com>
2023-06-16 23:10:22 +0300

    refactor: remove comment

--- migration-newip-to-ts/src/components/controller/appLoader.ts
+++ migration-newip-to-ts/src/components/controller/appLoader.ts
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
   constructor() {
     super('https://newsapi.org/v2/', {
-      apiKey: '6d2aae09fe9a4a16b0ca9602e26e2dbd', // получите свой ключ https://newsapi.org/
+      apiKey: '6d2aae09fe9a4a16b0ca9602e26e2dbd',
     });
   }
 }
